e5dcf330db02f0c0f5a4b7e4d39170a2c19a44ff
Taihenc <51960471+Taihenc@users.noreply.github.com>
2025-02-05 19:16:34 +0700

    ✅first page done

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import { twMerge } from 'tailwind-merge';
+
+type ButtonsProps = {
+    className?: string;
+    children?: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    onMouseDown?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    onMouseUp?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+// accept children & text props
+const Button: React.FC<ButtonsProps> = (props) => {
+    const _className = twMerge(
+        'bg-blue-500 hover:brightness-75 text-white font-bold py-3 px-3 rounded-xl cursor-pointer default-shadow',
+        props.className
+    );
+
+    return (
+        <div>
+            <button
+                className={_className}
+                onClick={props.onClick}
+                onMouseDown={props.onMouseDown}
+                onMouseUp={props.onMouseUp}
+            >
+                {props.children}
+            </button>
+        </div>
+    );
+};
+
+export default Button;
---
+++ src/config/variables.ts
@@ -0,0 +1,36 @@
+const options = [
+    {
+        text: 'ความโรแมนติก',
+        color: 'bg-[#fb7582] text-[#3e5466]',
+    },
+    {
+        text: 'ความอดทนและเข้าใจ',
+        color: 'bg-[#a5dcfe] text-[#3e5466]',
+    },
+    {
+        text: 'ความซื่อสัตย์',
+        color: 'bg-[#fbf1aa] text-[#3e5466]',
+    },
+    {
+        text: 'ความกล้าแสดงออก',
+        color: 'bg-[#64eb9c] text-[#3e5466]',
+    },
+    {
+        text: 'ความเป็นผู้ให้',
+        color: 'bg-[#b9fec1] text-[#3e5466]',
+    },
+    {
+        text: 'ความเป็นอิสระ',
+        color: 'bg-[#fec195] text-[#3e5466]',
+    },
+    {
+        text: 'ความอ่อนไหว',
+        color: 'bg-[#e5b8ff] text-[#3e5466]',
+    },
+    {
+        text: 'ความเป็นเจ้าของ',
+        color: 'bg-[#737df9] text-[#3e5466]',
+    },
+];
+
+export { options };
