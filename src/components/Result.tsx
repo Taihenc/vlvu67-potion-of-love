09f23954ef454fdebc72eec2ba9d25e36aad7652
Taihenc <51960471+Taihenc@users.noreply.github.com>
2025-02-08 19:16:56 +0700

    add logic for result page & rename img

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,29 +1,56 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import Button from './Button';
+import { ResultData, results } from '../config/variables';
+import { useEffect } from 'react';
 
 const Result: React.FC = () => {
     const navigate = useNavigate();
+
+    const [searchParams] = useSearchParams();
+    const urtype = searchParams.get('urtype'); // Get the 'urtype' param
+
+    let result: ResultData | undefined = undefined;
+    for (const res of results) {
+        if (res.id === urtype) {
+            result = res;
+            break;
+        }
+    }
+    useEffect(() => {
+        if (!result) {
+            console.error('Invalid urtype');
+            navigate('/'); // ✅ Navigates correctly inside useEffect
+        }
+    }, [result, navigate]);
+
     return (
-        <div className='w-full h-full flex flex-col gap-[3rem] justify-center items-center'>
-            <div className='w-[22rem] border-white border-[1rem] rounded-lg'>
-                <img src='../../public/ref/477994_0.jpg' alt='' />
-            </div>
-            <h1 className='relative text-3xl font-black text-[#fff4ba] h1-shadow-yellow drop-shadow-md'>
-                You value honestly!
-            </h1>
-            <div className='flex gap-5'>
-                <Button
-                    className='w-[10rem] bg-[#ef67ae]'
-                    onClick={() => navigate('/')}
-                >
-                    เริ่มต้นใหม่
-                </Button>
-                <Button
-                    className='w-[10rem] bg-[#fbf0ab] text-[#bcb374]'
-                    onClick={() => navigate('/')}
-                >
-                    กลับสู่หน้าหลัก
-                </Button>
+        <div className='w-full h-full flex justify-center items-center'>
+            <div className='w-[22rem] flex flex-col gap-[3rem] justify-center items-center  text-center'>
+                <div className='w-full border-white border-[1rem] rounded-lg'>
+                    <img src={`../../public/ref/${result?.image}`} alt='' />
+                </div>
+                <div>
+                    <h1 className='relative text-3xl font-black text-[#fff4ba] h1-shadow-yellow drop-shadow-md'>
+                        You value {result!.most_value}!
+                    </h1>
+                    <p className='mt-3 text-justify indent-[3ch]'>
+                        {result?.description}
+                    </p>
+                </div>
+                <div className='flex gap-5'>
+                    <Button
+                        className='w-[10rem] bg-[#ef67ae]'
+                        onClick={() => navigate('/')}
+                    >
+                        เริ่มต้นใหม่
+                    </Button>
+                    <Button
+                        className='w-[10rem] bg-[#fbf0ab] text-[#bcb374]'
+                        onClick={() => navigate('/')}
+                    >
+                        กลับสู่หน้าหลัก
+                    </Button>
+                </div>
             </div>
         </div>
     );
--- src/config/variables.ts
+++ src/config/variables.ts
@@ -69,6 +69,7 @@ export type ResultData = {
     score: Partial<Record<Options, number>>;
     image: string;
     description: string;
+    most_value: string;
 };
 
 export enum Results {
@@ -85,7 +86,7 @@ const results: ResultData[] = [
     {
         id: Results.romantic,
         name_th: 'คนรักที่มีความโรแมนติก',
-        name_en: 'Romantic Lover',
+        name_en: 'Eternal Romance',
         score: {
             romance: 50,
             honesty: 30,
@@ -94,11 +95,12 @@ const results: ResultData[] = [
         image: 'eternal_romance.jpg',
         description:
             'เน้นความโรแมนติกและการแสดงออกทางอารมณ์มากกว่าความสัมพันธ์ที่มีพื้นฐานจากการเป็นหุ้นส่วนทางจิตใจ ความซื่อสัตย์และความกล้าแสดงออกจะอยู่ในระดับที่พอเหมาะเพื่อเสริมความสัมพันธ์ให้มีสีสัน',
+        most_value: 'romance',
     },
     {
         id: Results.understanding,
         name_th: 'คนรักที่เข้าใจ',
-        name_en: 'Understanding Lover',
+        name_en: 'Embrace of Love',
         score: {
             patience: 40,
             giving: 30,
@@ -107,11 +109,12 @@ const results: ResultData[] = [
         image: 'embrace_of_love.jpg',
         description:
             'เป็นคนที่มีความอดทนสูงและยอมรับความแตกต่างของคู่รัก มีทัศนคติที่ยอมรับข้อบกพร่องของทั้งตัวเองและคู่รัก แต่ยังคงมีความซื่อสัตย์และพร้อมที่จะให้ในการสร้างความสัมพันธ์ที่ยั่งยืน',
+        most_value: 'patience',
     },
     {
         id: Results.warm_hearted,
         name_th: 'คนรักที่มีความอบอุ่น',
-        name_en: 'Warm-Hearted Lover',
+        name_en: 'Cozy Cuddle',
         score: {
             sensitivity: 50,
             honesty: 30,
@@ -120,11 +123,12 @@ const results: ResultData[] = [
         image: 'cozy_cuddle.jpg',
         description:
             'บุคคลที่มีความอ่อนไหวสูง มักจะมีความเข้าใจต่อความรู้สึกของคนอื่นและใส่ใจคู่รักอย่างมาก มีความซื่อสัตย์ที่แข็งแกร่งในความสัมพันธ์ แต่บางครั้งอาจทำให้รู้สึกอ่อนไหวเกินไปเมื่อเจอสถานการณ์ที่ยากลำบาก',
+        most_value: 'sensitivity',
     },
     {
         id: Results.independent,
         name_th: 'คนรักที่ต้องการอิสระ',
-        name_en: 'Independent Lover',
+        name_en: 'Love Refreshing',
         score: {
             independence: 60,
             romance: 20,
@@ -133,11 +137,12 @@ const results: ResultData[] = [
         image: 'love_refreshing.jpg',
         description:
             'คนที่ให้ความสำคัญกับความอิสระในความสัมพันธ์ ชอบความเป็นตัวของตัวเอง แต่ยังคงมีความโรแมนติกและความต้องการบางอย่างในการแสดงออกถึงความรัก อย่างไรก็ตามอาจมีความรู้สึกเป็นเจ้าของบ้างในบางสถานการณ์',
+        most_value: 'independence',
     },
     {
         id: Results.stable_assertive,
         name_th: 'คนรักที่มั่นคงและชัดเจน',
-        name_en: 'Stable & Assertive Lover',
+        name_en: 'Intense Amour',
         score: {
             bravery: 40,
             patience: 30,
@@ -146,11 +151,12 @@ const results: ResultData[] = [
         image: 'intense_amour.jpg',
         description:
             'คนประเภทนี้จะกล้าแสดงออกอย่างชัดเจนเกี่ยวกับความรู้สึกและความต้องการในความสัมพันธ์ แต่ก็สามารถอดทนและเข้าใจในสถานการณ์ต่างๆ ได้ดี แม้จะมีความเป็นเจ้าของและการควบคุมในบางครั้ง',
+        most_value: 'bravery',
     },
     {
         id: Results.protective,
         name_th: 'คนรักที่พร้อมปกป้อง',
-        name_en: 'Protective Lover',
+        name_en: 'Kandy Kiss',
         score: {
             ownership: 50,
             honesty: 30,
@@ -159,11 +165,12 @@ const results: ResultData[] = [
         image: 'kandy_kiss.jpg',
         description:
             'คนนี้อาจจะมีความรู้สึกเป็นเจ้าของคู่รักสูง จึงให้ความสำคัญกับความซื่อสัตย์และความมุ่งมั่นในความสัมพันธ์ ในขณะเดียวกันก็ไม่ลังเลที่จะเปิดเผยความรู้สึกและแสดงออกอย่างชัดเจนเพื่อปกป้องความสัมพันธ์',
+        most_value: 'ownership',
     },
     {
         id: Results.sensitive,
         name_th: 'คนรักที่ละเอียดอ่อน',
-        name_en: 'Sensitive Lover',
+        name_en: 'Heaven Heart',
         score: {
             sensitivity: 40,
             patience: 40,
@@ -172,6 +179,7 @@ const results: ResultData[] = [
         image: 'heaven_heart.jpg',
         description:
             'บุคคลนี้มีความอ่อนไหวและสามารถเข้าใจอารมณ์และความรู้สึกของคนอื่นได้ดี สามารถให้ความสำคัญกับคู่รักได้อย่างทุ่มเท แม้จะมีความเป็นอิสระบางประการ พวกเขามักให้ความสำคัญกับการสื่อสารและการเข้าใจในระดับลึก',
+        most_value: 'sensitivity',
     },
 ];
 
